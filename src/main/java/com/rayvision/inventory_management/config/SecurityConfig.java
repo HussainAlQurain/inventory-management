70f0e6acda8a6cf01dadc0fb035569feb05afb21
hussain-laptop <hussain.qurain@outlook.com>
2024-10-08 18:06:31 +0300

    add files to work on from PC

--- src/main/java/com/rayvision/inventory_management/config/SecurityConfig.java
+++ src/main/java/com/rayvision/inventory_management/config/SecurityConfig.java
@@ -1,28 +1,36 @@
 package com.rayvision.inventory_management.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/user").permitAll()
-                .anyRequest().authenticated()
-        ).formLogin(formLogin -> formLogin
-                .loginPage("/login").permitAll()
-        )
-                .rememberMe(Customizer.withDefaults());
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+        return new InMemoryUserDetailsManager();
+    }
 }
--- src/main/java/com/rayvision/inventory_management/controllers/UserController.java
+++ src/main/java/com/rayvision/inventory_management/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.rayvision.inventory_management.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

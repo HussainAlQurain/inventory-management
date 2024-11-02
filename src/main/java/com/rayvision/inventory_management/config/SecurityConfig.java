157841473c294546f5eae81ef77d094d290914d6
Hussain <g202316090@kfupm.edu.sa>
2024-11-02 20:26:02 +0300

    allow cors

--- src/main/java/com/rayvision/inventory_management/config/SecurityConfig.java
+++ src/main/java/com/rayvision/inventory_management/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rayvision.inventory_management.config;
 
 import com.rayvision.inventory_management.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,6 +22,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -32,11 +39,16 @@ public class SecurityConfig {
     @Autowired
     private JwtFilter jwtFilter;
 
+    @Value("${cors.allowed.origins}") // Inject the value from application.properties
+    private String corsAllowedOrigins;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults()) // Enables CORS configuration using a custom CorsConfigurationSource bean
                 .authorizeHttpRequests(request -> request.requestMatchers("users/login").permitAll().anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
@@ -62,4 +74,21 @@ public class SecurityConfig {
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(14);
     }
+
+    // CORS configuration bean
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // Parse the allowed origins from the property
+        List<String> allowedOrigins = Arrays.asList(corsAllowedOrigins.split(","));
+        corsConfiguration.setAllowedOrigins(allowedOrigins);
+        
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control", "X-Requested-With", "Origin", "Accept"));
+        corsConfiguration.setAllowCredentials(true); // Allow credentials like cookies or Authorization headers
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
 }

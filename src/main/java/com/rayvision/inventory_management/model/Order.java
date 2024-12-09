a6add9f172ded215cd0e8e4ec2edd8bab8cfc331
Hussain <g202316090@kfupm.edu.sa>
2024-12-10 00:00:27 +0300

    create two models for the database

--- src/main/java/com/rayvision/inventory_management/model/Order.java
+++ src/main/java/com/rayvision/inventory_management/model/Order.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
@@ -21,37 +22,39 @@ public class Order {
     @Id
     private long id;
     private String orderNumber;
+    private LocalDateTime creationDate;
     private LocalDate sentDate;
     private LocalDate deliveryDate;
 
     @ManyToOne
-    @JoinColumn(name = "buyer_id")
-    private Location buyer;
+    @JoinColumn(name = "buyer_location_id", nullable = false)
+    private Location buyerLocation; // The location that placed the order
 
     @ManyToOne
-    @JoinColumn(name = "supplier_id")
-    private Supplier supplier;
+    @JoinColumn(name = "sent_by_location_id", nullable = false)
+    private Location sentByLocation; // The location sending the order
+
+    @ManyToOne
+    @JoinColumn(name = "sent_to_supplier_id", nullable = false)
+    private Supplier sentToSupplier; // The supplier receiving the order
 
     private Double estimatedPrice;
 
     @Enumerated(EnumType.STRING)
-    private  OrderStatus status;
+    private OrderStatus status;
 
     @ManyToOne
-    @JoinColumn(name = "sent_by_user_id")
-    private Users sentBy;
-
-    @ManyToOne
-    @JoinColumn(name = "sent_to_user_id")
-    private Users sentTo;
+    @JoinColumn(name = "created_by_user_id", nullable = false)
+    private User createdByUser; // The user who created the order
 
     private String comments;
+
     private Double totalExcludingTax;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderHistory> orderHistory;
 
 }
---
+++ src/main/java/com/rayvision/inventory_management/model/OrderItem.java
@@ -0,0 +1,41 @@
+package com.rayvision.inventory_management.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+public class OrderItem {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "inventory_item_id", nullable = false)
+    private InventoryItem inventoryItem;
+
+    private String unitOfOrdering;
+
+    private String productCode;
+
+    private Double quantity;
+
+    private Double extendedQuantity; // Calculated as unit * quantity
+
+    private Double price;
+
+    private Double total;
+
+
+
+}

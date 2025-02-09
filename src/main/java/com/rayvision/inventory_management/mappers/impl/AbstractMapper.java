5065af46970fcf8216cb2a4323119185b25b872d
hussain-laptop <hussain.qurain@outlook.com>
2025-02-09 10:20:38 +0300

    create abstractmapper to reduce boilerplate for mappers

---
+++ src/main/java/com/rayvision/inventory_management/mappers/impl/AbstractMapper.java
@@ -0,0 +1,24 @@
+package com.rayvision.inventory_management.mappers.impl;
+
+import com.rayvision.inventory_management.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public abstract class AbstractMapper<E, D> implements Mapper<E, D> {
+    protected ModelMapper modelMapper;
+    public AbstractMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public D mapTo(E entity) {
+        return modelMapper.map(entity, getDtoClass());
+    }
+
+    @Override
+    public E mapFrom(D dto) {
+        return modelMapper.map(dto, getEntityClass());
+    }
+
+    protected abstract Class<E> getEntityClass();
+    protected abstract Class<D> getDtoClass();
+}
---
+++ src/main/java/com/rayvision/inventory_management/mappers/impl/InventoryItemCreateMapper.java
@@ -0,0 +1,25 @@
+package com.rayvision.inventory_management.mappers.impl;
+
+import com.rayvision.inventory_management.mappers.Mapper;
+import com.rayvision.inventory_management.model.InventoryItem;
+import com.rayvision.inventory_management.model.dto.InventoryItemCreateDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InventoryItemCreateMapper extends AbstractMapper<InventoryItem, InventoryItemCreateDTO> {
+
+    public InventoryItemCreateMapper(ModelMapper modelMapper) {
+        super(modelMapper);
+    }
+
+    @Override
+    protected Class<InventoryItem> getEntityClass() {
+        return InventoryItem.class;
+    }
+
+    @Override
+    protected Class<InventoryItemCreateDTO> getDtoClass() {
+        return InventoryItemCreateDTO.class;
+    }
+}

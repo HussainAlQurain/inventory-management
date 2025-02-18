64e7f4f34d7547ecb0c3156fd76f0896f48a9cae
hussain-laptop <hussain.qurain@outlook.com>
2025-02-18 13:35:28 +0300

    Add unitofmesaure controller

---
+++ src/main/java/com/rayvision/inventory_management/controllers/UnitOfMeasureController.java
@@ -0,0 +1,4 @@
+package com.rayvision.inventory_management.controllers;
+
+public class UnitOfMeasureController {
+}
---
+++ src/main/java/com/rayvision/inventory_management/model/dto/UnitOfMeasurePartialUpdateDTO.java
@@ -0,0 +1,4 @@
+package com.rayvision.inventory_management.model.dto;
+
+public class UnitOfMeasurePartialUpdateDTO {
+}

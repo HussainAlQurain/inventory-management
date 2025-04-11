f1f2296f6c72efbee0dd755fb4551ce1f43e95ea
Hussain <g202316090@kfupm.edu.sa>
2025-04-11 19:46:33 +0300

    enable scheduling

--- src/main/java/com/rayvision/inventory_management/InventoryManagementApplication.java
+++ src/main/java/com/rayvision/inventory_management/InventoryManagementApplication.java
@@ -3,10 +3,12 @@ package com.rayvision.inventory_management;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
+@EnableScheduling
 public class InventoryManagementApplication implements CommandLineRunner {
 
 	private static final Logger LOGGER = Logger.getLogger(InventoryManagementApplication.class.getName());
